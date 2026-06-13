3493f63ff124f0bf3abd68279f4258b9f3681f06
VinayKumar8374 <93717941+VinayKumar8374@users.noreply.github.com>
2026-06-14 00:27:40 +0530

    Create destinations.py

---
+++ data/destinations.py
@@ -0,0 +1,160 @@
+destinations = [
+
+{
+    "name": "Ladakh",
+    "location": "Jammu & Kashmir",
+    "category": "Mountain Adventure",
+    "image":
+    "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
+
+    "description":
+    """
+    Ladakh is one of India's most breathtaking destinations.
+    Famous for high altitude mountains, monasteries,
+    lakes and adventurous roads.
+    """,
+
+    "places": [
+        "Pangong Lake",
+        "Nubra Valley",
+        "Magnetic Hill",
+        "Thiksey Monastery"
+    ],
+
+    "best_time":
+    "May to September"
+},
+
+
+{
+    "name": "Goa",
+    "location": "Goa",
+    "category": "Beach Paradise",
+    "image":
+    "https://images.unsplash.com/photo-1512343879784-a960bf40e7f2",
+
+    "description":
+    """
+    Goa is India's most popular beach destination.
+    It offers beaches, nightlife, Portuguese architecture
+    and delicious seafood.
+    """,
+
+    "places":[
+        "Baga Beach",
+        "Calangute Beach",
+        "Fort Aguada",
+        "Dudhsagar Falls"
+    ],
+
+    "best_time":
+    "November to February"
+},
+
+
+{
+    "name":"Jaipur",
+    "location":"Rajasthan",
+    "category":"Heritage & Culture",
+
+    "image":
+    "https://images.unsplash.com/photo-1477587458883-47145ed94245",
+
+    "description":
+    """
+    Known as the Pink City, Jaipur represents India's
+    royal history through forts, palaces and traditions.
+    """,
+
+    "places":[
+        "Amber Fort",
+        "City Palace",
+        "Hawa Mahal",
+        "Jantar Mantar"
+    ],
+
+    "best_time":
+    "October to March"
+},
+
+
+{
+    "name":"Kerala Backwaters",
+    "location":"Kerala",
+
+    "category":"Nature Escape",
+
+    "image":
+    "https://images.unsplash.com/photo-1602216056096-3b40cc0c9944",
+
+    "description":
+    """
+    Kerala is famous for peaceful backwaters,
+    houseboats, greenery and authentic culture.
+    """,
+
+    "places":[
+        "Alleppey",
+        "Munnar",
+        "Kumarakom",
+        "Wayanad"
+    ],
+
+    "best_time":
+    "September to March"
+},
+
+
+{
+    "name":"Varanasi",
+    "location":"Uttar Pradesh",
+
+    "category":"Spiritual Journey",
+
+    "image":
+    "https://images.unsplash.com/photo-1561361513-2d000a50f0dc",
+
+    "description":
+    """
+    One of the world's oldest cities.
+    Varanasi is famous for temples,
+    ghats and spiritual experiences.
+    """,
+
+    "places":[
+        "Ganga Ghats",
+        "Kashi Vishwanath Temple",
+        "Sarnath"
+    ],
+
+    "best_time":
+    "October to March"
+},
+
+
+{
+    "name":"Andaman Islands",
+    "location":"Andaman & Nicobar",
+
+    "category":"Island Adventure",
+
+    "image":
+    "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+
+    "description":
+    """
+    Tropical islands with crystal clear water,
+    coral reefs and beautiful beaches.
+    """,
+
+    "places":[
+        "Radhanagar Beach",
+        "Havelock Island",
+        "Cellular Jail"
+    ],
+
+    "best_time":
+    "November to April"
+}
+
+]
